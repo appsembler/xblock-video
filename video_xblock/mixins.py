0423ff4d111254960c53b8ada6274c491329b245
Bryan Wilson <bryanlandia@gmail.com>
2017-09-01 12:54:55 -0700

    Fix transcript download request URL bug

--- video_xblock/mixins.py
+++ video_xblock/mixins.py
@@ -302,7 +302,7 @@ class TranscriptsMixin(XBlock):
         """
         trans_path = self.get_path_for(request.query_string)
         filename = self.get_file_name_from_path(trans_path)
-        transcript = requests.get(request.schema + '://127.0.0.1' + request.query_string).text
+        transcript = requests.get(request.scheme + '://127.0.0.1' + request.query_string).text
         response = Response(transcript)
         headerlist = [
             ('Content-Type', 'text/plain'),
