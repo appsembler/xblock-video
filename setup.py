b658f5eaa84bfe85724858c356b5fc6520221aab
imhassantariq <hassan.tariq@arbisoft.com>
2020-01-01 14:35:46 +0500

    Fixes for ironwood

--- setup.py
+++ setup.py
@@ -53,11 +53,10 @@ setup(
         'git+https://github.com/edx/xblock-utils.git@v1.0.5#egg=xblock-utils==1.0.5',
     ],
     install_requires=[
-        'pycaption>=0.7.1,<=1.0.1',  # The latest Python 2.7 compatible version
+        'le-pycaption',
         'requests>=2.9.1,<3.0.0',
         'babelfish>=0.5.5,<0.6.0',
         'XBlock>=0.4.10,<2.0.0',
-        'xblock-utils>=1.0.2,<=1.1.1'
     ],
     entry_points={
         'xblock.v1': [
