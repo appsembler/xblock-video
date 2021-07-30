91add98a5bcb2b0104f8159a712bb1a5cefd92b4
Bryan Wilson <bryanlandia@gmail.com>
2021-07-30 01:25:30 +0000

    Update xblock-utils requirement for Juniper compat

--- setup.py
+++ setup.py
@@ -48,15 +48,13 @@ setup(
         'video_xblock',
     ],
     dependency_links=[
-         # Replace dependency links with numbered versions when it's released on PyPI
-        'git+https://github.com/edx/xblock-utils.git@v1.0.5#egg=xblock-utils==1.0.5',
     ],
     install_requires=[
         'pycaption>=0.7.1,<=1.0.1',  # The latest Python 2.7 compatible version
         'requests>=2.9.1,<3.0.0',
         'babelfish>=0.5.5,<0.6.0',
         'XBlock>=0.4.10,<2.0.0',
-        'xblock-utils>=1.0.2,<=1.1.1'
+        'xblock-utils>2,<=2.1.1'
     ],
     entry_points={
         'xblock.v1': [
