88926a37c3630167dcf146adbf6baeacb7e2cbd3
Bryan Wilson <bryanlandia@gmail.com>
2021-08-02 14:10:55 -0700

    2to3 changes

--- video_xblock/backends/base.py
+++ video_xblock/backends/base.py
@@ -63,12 +63,10 @@ class BaseApiClient(object):
         """
 
 
-class BaseVideoPlayer(Plugin):
+class BaseVideoPlayer(Plugin, metaclass=abc.ABCMeta):
     """
     Inherit your video player class from this class.
     """
-
-    __metaclass__ = abc.ABCMeta
 
     entry_point = 'video_xblock.v1'
 
@@ -316,7 +314,7 @@ class BaseVideoPlayer(Plugin):
             return any(regex.search(href) for regex in cls.url_re)
         elif isinstance(cls.url_re, type(re.compile(''))):
             return cls.url_re.search(href)  # pylint: disable=no-member
-        elif isinstance(cls.url_re, basestring):
+        elif isinstance(cls.url_re, str):
             return re.search(cls.url_re, href, re.I)
 
     def add_js_content(self, path, **context):
