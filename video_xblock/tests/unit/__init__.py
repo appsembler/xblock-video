f4105ea907b107e8d77ec69aa6ef29536bab6721
Bryan Wilson <bryanlandia@gmail.com>
2021-08-02 13:04:51 -0700

    setup DjangoTemplates backend in tests

--- video_xblock/tests/unit/__init__.py
+++ video_xblock/tests/unit/__init__.py
@@ -1,6 +1,15 @@
 """
 Unit tests for video_xblock modules.
 """
+import django
 from django.conf import settings
 
-settings.configure()
+settings.configure(
+
+    TEMPLATES=[
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        },
+    ]
+)
+django.setup()
